f1acc780b6d0979849e3f1a6773adb2008bf5904
Wullems Bart <wullemsb@gmail.com>
2022-11-23 19:16:49 +0100

    Add project files.

---
+++ EventCountersDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Tracing;
+using System.Xml.Linq;
+
+
+var random = new Random(0);
+Task runMigration = new Task(() =>
+{
+    while (true)
+    {
+        Thread.Sleep(1000);
+
+        MigratedRecordsCounterSource.Log.Add(random.Next(1,5));
+    }
+
+});
+
+runMigration.Start();
+
+Console.ReadLine();
+
+
+
+[EventSource(Name = "Migrator.MigratedRecordsCounter")]
+public sealed class MigratedRecordsCounterSource : EventSource
+{
+    public static readonly MigratedRecordsCounterSource Log = new();
+    private PollingCounter _migratedRecordsCounter;
+    private long _migratedRecordsCount = 0;
+
+    private MigratedRecordsCounterSource()
+    {
+        _migratedRecordsCounter = new PollingCounter("migrated-records-count", this, () => Interlocked.Read(ref _migratedRecordsCount))
+        {
+            DisplayName = "Migration Count"
+        };
+    }
+
+    //See https://learn.microsoft.com/en-us/dotnet/core/diagnostics/event-counters#concurrency
+    public void Add(int count) => Interlocked.Add(ref _migratedRecordsCount, count);
+
+    protected override void Dispose(bool disposing)
+    {
+        _migratedRecordsCounter?.Dispose();
+        _migratedRecordsCounter = null;
+
+        base.Dispose(disposing);
+    }
+}
